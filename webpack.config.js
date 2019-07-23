16208b2988bdc86e5ada9f2a7d1fc4e9cd8754ff
shemaeric <shemaeric01@gmail.com>
2019-07-23 14:10:42 +0200

    setup the angular app with webpack

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/main.ts",
+  resolve: {
+    extensions: [".ts", ".js"]
+  },
+  module: {
+    rules: [
+      {
+        /* look for all the files with extension ts and transform them using ts-loader
+        * and angular2-template-loader
+        */ 
+        test: /\.ts$/,
+        use: ["ts-loader", "angular2-template-loader"]
+      },
+      {
+
+        /* look for all the files with extension 
+        * html and css to transform them using raw-loader
+        */ 
+        test: /\.(html|css)$/,
+        use: "raw-loader"
+      }
+    ]
+  },
+  plugins: [
+    // inject JavaScript bundle output outputs to the index.html
+    new HtmlWebpackPlugin({ template: "./src/index.html" })
+  ]
+};
