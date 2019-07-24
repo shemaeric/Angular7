d3b6168fb9d204ae198cf4e2a6b88bf577b67a44
shemaeric <shemaeric01@gmail.com>
2019-07-24 12:04:03 +0200

    add the login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
 
-@Component({ templateUrl: 'login.component.html' })
+@Component(
+    { templateUrl: 'login.component.html',
+    styleUrls: ['login.component.css']
+     }
+)
 export class LoginComponent {}
